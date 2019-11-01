2c153c93aa37a313c9d2836bf356dc0aeb1537fe
grandemayta <grandemayta@gmail.com>
2019-11-01 14:05:15 +0100

    2 files modified

--- components/standings.ts
+++ components/standings.ts
@@ -8,9 +8,13 @@ export class Standings extends HTMLElement {
   async connectedCallback() {
     const competition = this.competition.toUpperCase();
     const season = "2019";
-    const standings: StandingsRes = await getStandings({ competition, season });
-    console.log(standings);
-    standings.
+    const standings: [StandingsRes] = await getStandings({ competition, season });
+    const findTeam = standings.findIndex(standing => {
+      const team = standing.team.name.toLowerCase();
+      return team.includes(this.team.toLowerCase());
+    });
+
+    console.log(findTeam);
 
     this.innerHTML = `
       <table>
--- services/http-wrapper.ts
+++ services/http-wrapper.ts
@@ -1,11 +1,11 @@
 import { BASEURL, HEADERS } from '../constants';
-import { StandingsReq, StandingsRes } from '../models';
+import { StandingsReq } from '../models';
 
 const httpWrapper = endpoint => {
     return fetch(`${BASEURL}/${endpoint}`, { headers: HEADERS });
 };
 
-export const getStandings: StandingsRes = async (req: StandingsReq) => {
+export const getStandings = async (req: StandingsReq) => {
     const endpoint = `competitions/${req.competition}/standings?season=${req.season}`;
     const response = await httpWrapper(endpoint);
     const json = await response.json();
