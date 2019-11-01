33d64d2b5fc76c7cae725f56d29722268f12dd37
grandemayta <grandemayta@gmail.com>
2019-11-01 11:22:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# favourite-football-team
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/favourite-football-team)
---
+++ index.ts
@@ -0,0 +1,9 @@
+class FootballTeams extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <h1>It works</h1>
+    `;
+  }
+}
+
+customElements.define("gm-football", FootballTeams);
