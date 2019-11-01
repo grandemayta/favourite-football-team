c89ae7ff5660c60e42ca73930fc6fc3b408bad70
grandemayta <grandemayta@gmail.com>
2019-11-01 19:11:44 +0100

    1 file deleted

--- services/get-team-id.ts
+++
@@ -1,7 +0,0 @@
-export const getTeamId = async (compeition: string) => {
-    const endpoint = `competitions/${competition}/teams}`;
-    const response = await httpWrapper(endpoint);
-    const json = await response.json();
-    return json.standings[0].table;
-};
-competitions/SA/teams
