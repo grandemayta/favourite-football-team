8fe11be30eed396e667fdbc661033cb97defac5a
grandemayta <grandemayta@gmail.com>
2019-11-01 11:57:20 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 export class FootballTeams extends HTMLElement {
   get competition() { return this.getAttribute('competition'); }
   get team() { return this.getAttribute('team'); }
-  
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
   connectedCallback() {
-    this.innerHTML = `
-      <h1>It works</h1>
+    this.shadowRoot.innerHTML = `
+      <h1>STANDINGS</h1>
+      <h1>NEXT MATCHES</h1>
+      <h1>PLAYERS SCORES</h1>
     `;
   }
 }
