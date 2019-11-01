3c7739aef331fd56d69589503e427f0570cb3476
grandemayta <grandemayta@gmail.com>
2019-11-01 16:35:58 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -16,8 +16,7 @@ export class FootballTeams extends HTMLElement {
 
   async render() {
     const competition = this.competition.toUpperCase();
-    const teams = await getTeams(competition);
-    console.log(teams);
+    
     this.shadowRoot.innerHTML = `
       <style>
         * {
@@ -37,7 +36,7 @@ export class FootballTeams extends HTMLElement {
         }
       </style>
       <h1>STANDINGS</h1>
-      <gm-standings competition=${competition} team=${this.team}></gm-standings>
+      <gm-standings competition="${competition}" team="${this.team}"></gm-standings>
       <h1>NEXT MATCHES</h1>
       <h1>PLAYERS SCORES</h1>
     `;
